d9ad83b349db03c77c3d35720fc7d23d2570dc04
Nicolas Chapgier <nchapgier@hotmail.fr>
2022-11-23 14:58:52 +0100

    loop for dark-mode

--- javascript.js
+++ javascript.js
@@ -1,21 +1,24 @@
 let switchBox = document.querySelector(".dark-mode");
 let btn = document.querySelector(".btn");
 let body = document.querySelector("body");
-let imgShadow = document.querySelector(".flex-container div img")
+let imgShadow = document.querySelectorAll(".img-shadow")
 let moon = document.querySelector(".fa-moon");
 let sun = document.querySelector(".fa-sun");
 
 // console.log(switchBox);
 // console.log(btn);
-console.log(moon);
-console.log(sun);
+// console.log(moon);
+// console.log(sun);
+console.log(imgShadow);
 
 switchBox.addEventListener('click',function(){
-    console.log("spouew");
+    // console.log("spouew");
     btn.classList.toggle("btn-change");
     body.classList.toggle("body-change");
     moon.classList.toggle("fa-moon-change");
     sun.classList.toggle("fa-sun-change");
-    imgShadow.classList.toggle("img-shadow-change");
+    for (let i = 0; i < imgShadow.length; i++) {
+        imgShadow[i].classList.toggle("img-shadow-change");
+    }
 })
 
