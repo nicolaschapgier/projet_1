c6a24406c114b1c9c9fc1f2ff8b006bf092d42d0
Nicolas Chapgier <nchapgier@hotmail.fr>
2022-12-01 12:41:23 +0100

    add javascript transition on the body

--- javascript.js
+++ javascript.js
@@ -57,3 +57,12 @@ function change(value1){
 for (let i = 0; i< allCards.length;i++){
   allCards[i].addEventListener("click", () => change(allParagraphe[i]));
 }
+
+
+h1.addEventListener("click",function(){
+  setTimeout(function() {
+  body.style.opacity = "0.3";
+  body.style.transform = "translateX(-100%)";
+  body.style.transition = "2s";
+  }, 1000);
+})
